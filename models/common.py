14721e947be6269973f14087216980c55ed743d5
VvvvvGH <cheng123xp@gmail.com>
2020-06-30 20:19:33 +0800

    remove slice

--- models/common.py
+++ models/common.py
@@ -84,7 +84,7 @@ class Focus(nn.Module):
         self.conv = Conv(c1 * 4, c2, k, 1)
 
     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
-        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
+        return self.conv(torch.cat([x,x,x,x], 1))
 
 
 class Concat(nn.Module):
